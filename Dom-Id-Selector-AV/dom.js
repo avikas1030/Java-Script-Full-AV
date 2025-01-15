1b26a54f376960990d8ab00e90d2da1e81aaa2e6
Vikas Choudhary <vikashbbn82@gmail.com>
2025-01-15 12:05:07 +0530

    Add files via upload
    DOM Tutorial Id Selector

---
+++ Dom-Id-Selector-AV/dom.js
@@ -0,0 +1,9 @@
+function design(){
+    var h1 = document.getElementById("name");
+    h1.style.color = "blue";
+    h1.style.fontSize = "50px";
+}
+function designweb(){
+    var body = document.getElementById("webPage");
+    body.style.animation = "demo 10s infinite";
+}
